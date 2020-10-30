27b5f7213a9f3cfd47d0162b53c605ac3836e523
visakht <visakht@xanthus.in>
2020-10-30 12:19:41 +0530

    First Commit

---
+++ JenkinsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsSample
+//
+//  Created by Xanthus Software Solutions on 30/10/20.
+//  Copyright © 2020 Xanthus Software Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
